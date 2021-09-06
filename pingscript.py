9e044b1657c3dd69de77bfbbd1986256b19ed0dc
Owen Hardy <owen.hardy@live.com>
2021-09-06 14:02:58 -0400

    Update pingscript.py

--- pingscript.py
+++ pingscript.py
@@ -1,13 +1,19 @@
-import os
-
-ip_addr = input('10.10.10.1')
-
-stream = os.popen('ping -c 4 {}'.format(ip_addr)) 
-output = stream.read() 
-
-if '0 received' in output: 
-  print('IP unreachable') 
-else: 
-  print('IP reachable')
-  
-print(output)
+import subprocess
+import platform
+ 
+def ping_ip(current_ip_address):
+        try:
+            output = subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower(
+            ) == "windows" else 'c', current_ip_address ), shell=True, universal_newlines=True)
+            if 'unreachable' in output:
+                return False
+            else:
+                return True
+        except Exception:
+                return False
+ 
+if __name__ == '__main__':
+        if ping_ip('10.10.10.1'):
+            print(f"{each} is available")
+        else:
+            print(f"{each} is not available")
