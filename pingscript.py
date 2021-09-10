c61646acf4f8fbcf362d5a7395fac029165a9396
Owen Hardy <owen.hardy@live.com>
2021-09-10 12:01:21 -0400

    Update pingscript.py

--- pingscript.py
+++ pingscript.py
@@ -1,29 +1,10 @@
-import subprocess
-import platform
- 
-IP = '10.10.10.1'
+import os
+from itertools import repeat
+
 n=500
+ip = "10.10.10.1"
 
+for i in repeat(None, n):
+    exit_code = os.system(f"ping -c 1 -w2 {ip} > /dev/null 2>&1")
+    print(exit_code == 0)
 
-
-def ping_ip(current_ip_address
-        try:
-            output = subproces
-            ) == "windows" els
-            if 'unreachable' i
-                return False
-            else:
-                return True
-        except Exception:
-                return False
- 
-if __name__ == '__main__':
-    for z in range(n):
-        if ping_ip(IP):
-            print(f"{IP} is av
-        else:
-            print(f"{IP} is no
-            
-print(f"Succesfully Pinged {IP
-#for z in range(n):
-   #bz = ping_ip('10.10.10.1')
