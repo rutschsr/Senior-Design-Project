976b0f07d0e8d969d00ebe4410e50f73ddce9abe
Sam Rutschilling <56640827+rutschsr@users.noreply.github.com>
2021-09-13 18:26:24 -0400

    Timing Added
    Timing Added

--- pingscript.py
+++ pingscript.py
@@ -1,10 +1,19 @@
 import os
 from itertools import repeat
+from datetime import datetime
 
 n=500
 ip = "10.10.10.1"
 
+starttime=datetime.now()
+
 for i in repeat(None, n):
     exit_code = os.system(f"ping -c 1 -w2 {ip} > /dev/null 2>&1")
     print(exit_code == 0)
+finishtime=datetime.now()
+print('Start time: ',str(starttime))
+print('Finish Time is: ',str(finishtime))
+totaltime=finishtime-starttime
+print('Total Elapsed Time Was: ', str(totaltime))
+
 
