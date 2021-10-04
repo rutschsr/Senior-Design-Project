1391bf096aa625813e9c63c1ba2eb3450e6581b6
Jordan Smith <77807597+Jord-Smi@users.noreply.github.com>
2021-10-04 16:54:58 -0400

    A simple script to count for checking power draw

---
+++ Raspberry Pi Test Scripts/count_script.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime
+
+n = 500
+
+starttime = datetime.now()
+i = 0
+while i < n:
+	print(i)
+	i = i+1
+
+finishtime = datetime.now()
+print('Start time: ', str(starttime))
+print('End time: ', str(finishtime))
+print('Total time: ', str(starttime - finishtime))
