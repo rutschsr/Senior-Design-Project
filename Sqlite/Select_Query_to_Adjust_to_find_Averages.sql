e66ccc40aab4ab94ed40b4b228ca8992ca2630b4
Jordan Smith <77807597+Jord-Smi@users.noreply.github.com>
2021-10-05 15:10:41 -0400

    Average Data Collection

--- Sqlite/Select_Query_to_Adjust_to_find_Averages.sql
+++ Sqlite/Select_Query_to_Adjust_to_find_Averages.sql
@@ -1,15 +1,10 @@
-/* I don't think SQLite has the capability to do all of the calculations we need like other database IDEs I've used
-or, if it does, I can't find anything about it. So, calculations after finding the averages are by hand for now. */
+--For 500test100Power
 
 -- The average of the ping current values, this will be the second value displayed
-SELECT avg(current) FROM current 
-	WHERE strftime('%H %M %S %f', date) BETWEEN strftime('%H %M %S %f', '2021-09-27 17:30:37.858201') AND strftime('%H %M %S %f', '2021-09-27 17:30:43.258201')
+SELECT avg(wattage) FROM PowerMeasurement 
+	WHERE strftime('%H %M %S %f', date) BETWEEN strftime('%H %M %S %f', '2021-09-30 11:08:04.194447') AND strftime('%H %M %S %f', '2021-09-30 11:08:13.729347')
 UNION
 -- The average of the nonping current values, this will be the first value displayed
-SELECT avg(current) FROM current 
-	WHERE strftime('%H %M %S %f', date) NOT BETWEEN strftime('%H %M %S %f', '2021-09-27 17:30:37.858201') AND strftime('%H %M %S %f', '2021-09-27 17:30:43.258201');
-
-/* These queries can be easily manipulated to work for every set of data we ever make, it's a simple matter of changing the column 
-name and table name to power (or whatever we decide to call it when we start using power instead) and changing the dates to fit 
-where the pings occur. */
+SELECT avg(wattage) FROM PowerMeasurement 
+	WHERE strftime('%H %M %S %f', date) NOT BETWEEN strftime('%H %M %S %f', '2021-09-30 11:08:04.194447') AND strftime('%H %M %S %f', '2021-09-30 11:08:13.729347');
 
