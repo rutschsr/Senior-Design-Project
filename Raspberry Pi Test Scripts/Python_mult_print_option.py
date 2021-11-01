a6de085a4b0610f3bc588299184682332d584a09
Sam Rutschilling <56640827+rutschsr@users.noreply.github.com>
2021-11-01 17:34:32 -0400

    11/1 In Lab Data

--- Raspberry Pi Test Scripts/Python_mult_print_option.py
+++ Raspberry Pi Test Scripts/Python_mult_print_option.py
@@ -1,7 +1,17 @@
 # Infinite multiplying loop
+from datetime import datetime
+import time
+
 i = 1
+n=1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+starttime = datetime.now()
 print("Start")
-while i < 2000:
+while i < n:
 	i = i * 2
-	# print(i)
+	print(i)
 print("End")
+
+finishtime = datetime.now()
+print('Start time: ', str(starttime))
+print('End time: ', str(finishtime))
+print('Total time: ', str(finishtime - starttime))
