f326ed84e51ce51f8cbaea2413c7c9d52ebaa3f6
Jordan Smith <77807597+Jord-Smi@users.noreply.github.com>
2022-02-08 16:20:49 -0500

    Update current time pulling

--- Raspberry Pi Test Scripts/C_mult_print_option.c
+++ Raspberry Pi Test Scripts/C_mult_print_option.c
@@ -2,9 +2,22 @@
 int main() {
 	int i = 1;
 	printf("Start");
+	
+	time_t t;
+	time_t time1=0;
+
+  	time ( &t );
+  	printf ( "Current local time and date: %d\n", ctime(&t));
+  	time1=t;
+	
 	while (i < 2000) {
 		// printf("%d", i);
 		i = i * 2;
 	}
 	printf("End");
+	
+	time ( &t );
+	// timeinfo = localtime ( &rawtime );
+  	printf ( "Current local time and date: %d\n", ctime(&time1);
+  	printf ( "Current local time and date: %d\n", ctime(&t));
 }
