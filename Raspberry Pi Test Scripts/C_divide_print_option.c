6c3b69060890ebe3efe2308c2c05f44e9f707a55
Jordan Smith <77807597+Jord-Smi@users.noreply.github.com>
2022-02-08 16:18:42 -0500

    Update current time pulling

--- Raspberry Pi Test Scripts/C_divide_print_option.c
+++ Raspberry Pi Test Scripts/C_divide_print_option.c
@@ -2,9 +2,23 @@
 int main() {
 	int i = 10000000;
 	printf("Start");
+	
+	time_t t;
+	time_t time1=0;
+
+  	time ( &t );
+  	printf ( "Current local time and date: %d\n", ctime(&t));
+  	time1=t;
+
 	while (i > 1) {
 		// printf("%d", i);
 		i = i / 2;
 	}
 	printf("End");
+	
+	
+	time ( &t );
+	// timeinfo = localtime ( &rawtime );
+  	printf ( "Current local time and date: %d\n", ctime(&time1);
+  	printf ( "Current local time and date: %d\n", ctime(&t));
 }
