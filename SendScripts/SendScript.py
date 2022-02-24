913c5ed5b4834d8678f1535c512bb330927e606f
Sam Rutschilling <56640827+rutschsr@users.noreply.github.com>
2022-02-24 10:16:25 -0500

    Create SendScript.py

---
+++ SendScripts/SendScript.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import subprocess
+import random
+import operator
+import csv
+import logging
+import tempfile
+
+def run_command(command):
+    print('----------------------------------------')
+    print('Executing: '+command)
+    pipe = subprocess.Popen(command, shell=True, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    #pipe.stdout.close()
+    out, err = pipe.communicate()
+    print (out)
+    
+
+
+print ("Number of arguments: %d" %  len(sys.argv))
+print ("Argument List: %s" % str(sys.argv))
+
+files_to_compile = ['stop_light_nice']
+directories = ['']
+file_to_compile = 'stop_light_nice'
+
+
+if len(sys.argv) == 2:
+    TrialCmd = 'null'
+else:
+	TrialCmd = sys.argv[2]
+
+numTrials = int(sys.argv[1])
+
+i=0
+
+while i < numTrials:
+    run_command(TrialCmd)
+
+
+
