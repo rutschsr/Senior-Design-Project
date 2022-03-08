e42927a15ed62a48a09a3c43130ff11dbb683ae8
Sam Rutschilling <56640827+rutschsr@users.noreply.github.com>
2022-03-08 17:31:25 -0500

    files

--- Datalogging Program/arduino_to_sqlite.py
+++ Datalogging Program/arduino_to_sqlite.py
@@ -8,7 +8,7 @@ baud = 115200 #Needs to be the same as the arudino otherwise it will not work
 fileName="current-data.csv" #name of the CSV file generated
 dbName="data.db"
 i=1
-con = sqlite3.connect(r'C:\Users\NBSwi\Documents\GitHub\Senior-Design-Project\Sqlite\M_full_png_from_PI.sqlite3')
+con = sqlite3.connect(r'C:\Users\NBSwi\Documents\GitHub\Senior-Design-Project\Sqlite\M_full_bmp_remote_to_local_from_PI.sqlite3')
 cur = con.cursor()
 try: #Add another column here for voltage: , voltage real
     cur.execute('''CREATE TABLE PowerMeasurement
