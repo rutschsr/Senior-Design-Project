c3edb553d9290b9b1c5d07f5f9ca02c8649939b0
Sam Rutschilling <56640827+rutschsr@users.noreply.github.com>
2022-04-19 17:37:58 -0400

    Update RepeatScript.py

--- DesignExpo/RepeatScript.py
+++ DesignExpo/RepeatScript.py
@@ -32,15 +32,30 @@ i=0
 
 
 TrialCMD1 = 'sshpass -p "rutschsr" scp 44mb.rtf sam@10.10.10.4:'
+TrialCMD2= 'sshpass -p "rutschsr" scp sam@10.10.10.4/44mb.rtf .'
 
 while True:
 
+    print()
+    print()
+    print()
+    print("Test Pinging Web Address:")
+    print()
+    print()
+
+    time.sleep(5)
     
-    while i<50:
+    while i<500:
         exit_code = os.system("ping -c 1 -w2 srutschilling.net")
         print(exit_code)
         i=i+1
 
+    print()
+    print()
+    print()
+    print("Test Sending Images To Remote:")
+    print()
+    print()
 
     time.sleep(5)
         
@@ -49,6 +64,24 @@ while True:
         run_command(TrialCMD1)
         i=i+1
     i=0
+
+    print()
+    print()
+    print()
+    print("Test Recieving Images From Remote:")
+    print()
+    print()
+    time.sleep(5)
+
+
+    
+    while i<20:
+        run_command(TrialCMD2)
+        i=i+1
+
+    i=0
+    
+    
     
     
 
