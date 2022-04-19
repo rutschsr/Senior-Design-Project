d5fabaf45da6a90de109397cefadf497f8578a72
Sam Rutschilling <56640827+rutschsr@users.noreply.github.com>
2022-04-19 16:57:57 -0400

    Update RepeatScript.py

--- DesignExpo/RepeatScript.py
+++ DesignExpo/RepeatScript.py
@@ -30,7 +30,7 @@ print ("Argument List: %s" % str(sys.argv))
 i=0
 
 
-TrialCMD1 = 'sshpass -p "raspberry" scp 44mb.rtf pi@10.10.10.4:'
+TrialCMD1 = 'sshpass -p "rutschr" scp 44mb.rtf sam@10.10.10.4:'
 
 while True:
 
